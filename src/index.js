2cd41036c55ed433f82711586f12df14e76163d8
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-07-26 11:24:35 +0300

    Update development

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import './style.css'
-import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'My Project'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -28,7 +28,7 @@ module.exports = {
       },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
 };
